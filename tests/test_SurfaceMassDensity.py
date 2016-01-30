ac0e0ca4880681ea886c8dc1e28c71e5d658a70d
Jes Ford <jesford@uw.edu>
2016-01-29 20:26:08 -0800

    tests use offsets keyword

--- tests/test_SurfaceMassDensity.py
+++ tests/test_SurfaceMassDensity.py
@@ -21,12 +21,12 @@ def test_rs_units():
 
     #test that a dimensionless rs is coverted to Mpc
     smd = SurfaceMassDensity(rs_unitless, delta_c, rho_c,
-                             sig_offset = sig_off, rbins = rbinarray)
+                             offsets = sig_off, rbins = rbinarray)
     assert_equal(smd._rs.unit, units.Mpc)
 
     #test that a non-Mpc unit on rs raises an error
     assert_raises(ValueError, SurfaceMassDensity, rs_wrongunits, delta_c,
-                  rho_c, sig_offset = sig_off, rbins = rbinarray)
+                  rho_c, offsets = sig_off, rbins = rbinarray)
 
 def test_rhoc_units():
     rhoc_unitless = rho_c.value
@@ -34,17 +34,17 @@ def test_rhoc_units():
 
     #test that a dimensionless rho_c is coverted to Msun/Mpc/pc**2
     smd = SurfaceMassDensity(r_s, delta_c, rhoc_unitless,
-                             sig_offset = sig_off, rbins = rbinarray)
+                             offsets = sig_off, rbins = rbinarray)
     assert_equal(smd._rho_crit.unit, units.Msun/units.Mpc/units.pc**2)
 
     #test that an incorrect unit on rho_c raises an error
     assert_raises(ValueError, SurfaceMassDensity, r_s, delta_c,
-                  rhoc_wrongunits, sig_offset = sig_off, rbins = rbinarray)
+                  rhoc_wrongunits, offsets = sig_off, rbins = rbinarray)
 
 def test_dc_units():
     dc_wrongunits = delta_c * units.Mpc
     assert_raises(ValueError, SurfaceMassDensity, r_s, dc_wrongunits, rho_c,
-                  sig_offset = sig_off, rbins = rbinarray)
+                  offsets = sig_off, rbins = rbinarray)
 
     
 #------------------------------------------------------------------------------
@@ -58,56 +58,56 @@ rbin_list = list(rbinarray.value)
 
 def test_list_rs():
     smd = SurfaceMassDensity(rs_list, delta_c, rho_c,
-                             sig_offset = sig_off, rbins = rbinarray)
+                             offsets = sig_off, rbins = rbinarray)
     assert_equal(smd._rs, r_s)
 
 def test_list_dc():
     smd = SurfaceMassDensity(r_s, dc_list, rho_c,
-                             sig_offset = sig_off, rbins = rbinarray)
+                             offsets = sig_off, rbins = rbinarray)
     assert_equal(smd._delta_c, delta_c)
 
 def test_list_rc():
     smd = SurfaceMassDensity(r_s, delta_c, rc_list,
-                             sig_offset = sig_off, rbins = rbinarray)
+                             offsets = sig_off, rbins = rbinarray)
     assert_equal(smd._rho_crit, rho_c)
 
 def test_list_sigoff():
     smd = SurfaceMassDensity(r_s, delta_c, rho_c,
-                             sig_offset = soff_list, rbins = rbinarray)
+                             offsets = soff_list, rbins = rbinarray)
     assert_equal(smd._sigmaoffset, sig_off)
 
 def test_list_rbins():
     smd = SurfaceMassDensity(r_s, delta_c, rho_c,
-                             sig_offset = sig_off, rbins = rbin_list)
+                             offsets = sig_off, rbins = rbin_list)
     assert_equal(smd._rbins, rbinarray)
 
 
 def test_input_single_values():
     assert_raises(TypeError, SurfaceMassDensity, r_s[0], delta_c, rho_c,
-                  sig_offset = sig_off, rbins = rbinarray)
+                  offsets = sig_off, rbins = rbinarray)
     assert_raises(TypeError, SurfaceMassDensity, r_s, delta_c[0], rho_c,
-                  sig_offset = sig_off, rbins = rbinarray)
+                  offsets = sig_off, rbins = rbinarray)
     assert_raises(TypeError, SurfaceMassDensity, r_s, delta_c, rho_c[0],
-                  sig_offset = sig_off, rbins = rbinarray)
+                  offsets = sig_off, rbins = rbinarray)
     assert_raises(TypeError, SurfaceMassDensity, r_s, delta_c, rho_c,
-                  sig_offset = sig_off[0], rbins = rbinarray)
+                  offsets = sig_off[0], rbins = rbinarray)
     assert_raises(TypeError, SurfaceMassDensity, r_s, delta_c, rho_c,
-                  sig_offset = sig_off, rbins = rbinarray[0])
+                  offsets = sig_off, rbins = rbinarray[0])
 
     
 def test_incompatible_lengths():
 
     assert_raises(ValueError, SurfaceMassDensity, r_s[0:2], delta_c, rho_c,
-                  sig_offset = sig_off, rbins = rbinarray)
+                  offsets = sig_off, rbins = rbinarray)
     
     assert_raises(ValueError, SurfaceMassDensity, r_s, delta_c[0:2], rho_c,
-                  sig_offset = sig_off, rbins = rbinarray)
+                  offsets = sig_off, rbins = rbinarray)
 
     assert_raises(ValueError, SurfaceMassDensity, r_s, delta_c, rho_c[0:2],
-                  sig_offset = sig_off[0:2], rbins = rbinarray)
+                  offsets = sig_off[0:2], rbins = rbinarray)
 
     assert_raises(ValueError, SurfaceMassDensity, r_s, delta_c, rho_c,
-                  sig_offset = sig_off[0:2], rbins = rbinarray)
+                  offsets = sig_off[0:2], rbins = rbinarray)
 
 
 #------------------------------------------------------------------------------
@@ -218,7 +218,7 @@ def _test_miscentered_profiles():
     for i, (r_s, delta_c, rho_c) in enumerate(zipped_inputs):
             
         smd = SurfaceMassDensity(r_s, delta_c, rho_c,
-                                 sig_offset = toy_data_offset,
+                                 offsets = toy_data_offset,
                                  rbins = toy_data_rbins)
         sigma_py_off = smd.sigma_nfw()
         _check_sigma(i)
