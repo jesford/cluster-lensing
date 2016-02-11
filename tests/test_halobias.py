fef582974385f6ce7daa3bc1021cc179bfa2c38e
Jes Ford <jesford@uw.edu>
2016-02-11 12:08:00 -0800

    make test_halobias.py flake8 happy

--- tests/test_halobias.py
+++ tests/test_halobias.py
@@ -1,23 +1,24 @@
 import numpy as np
-from numpy.testing import assert_equal, assert_allclose
+from numpy.testing import assert_allclose
 from astropy.cosmology import Planck13 as cosmo
 
 import halobias
+
 
 def test_Mnl_z0():
     z = 0.
     h = cosmo.h
 
-    def _check_bias_z0(m,ans):
-        b = halobias.bias(m,z)
-        assert_allclose(b,ans)
+    def _check_bias_z0(m, ans):
+        b = halobias.bias(m, z)
+        assert_allclose(b, ans)
 
-    #integer multiples of non-linear mass
+    # integer multiples of non-linear mass
     M_nl = (8.73/h) * 10.**12.
-    mass = np.array([0.,1.,2.,10., 100.]) * M_nl
-    
-    #corresponding bias at z=0
+    mass = np.array([0., 1., 2., 10., 100.]) * M_nl
+
+    # corresponding bias at z=0
     answer = [0.66, 0.9236707, 1.06577485, 1.64530492, 4.127912607]
-    
+
     for m, ans in zip(mass, answer):
-        yield  _check_bias_z0, m, ans
+        yield _check_bias_z0, m, ans
