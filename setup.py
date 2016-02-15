59091a8c2bb76860d52eac5214166eb57ec013a7
Jes Ford <jesford@uw.edu>
2016-02-14 21:00:50 -0800

    change project, package name to all lowercase letters

--- setup.py
+++ setup.py
@@ -4,40 +4,47 @@ try:
 except ImportError:
     from distutils.core import setup
 
-DESCRIPTION = "Galaxy Cluster Properties and Weak Lensing Profiles"
+DESCRIPTION = "Galaxy Cluster and Weak Lensing Tools"
 LONG_DESCRIPTION = """
-cluster-lensing: galaxy cluster halo calculations
+clusterlensing: galaxy cluster halo calculations
 ======================================================
 This package includes tools for calculating a variety of galaxy cluster properties, as well as mass-richness and mass-concentration scaling relations, and weak lensing profiles. These include surface mass density (Sigma) and differential surface mass density (DeltaSigma) for NFW halos, both with and without the effects of cluster miscentering.
-For more information, visit http://github.com/jesford/cluster-lensing
+For more information, visit http://clusterlensing.readthedocs.org/
 """
-NAME = "cluster-lensing"
+NAME = "clusterlensing"
 AUTHOR = "Jes Ford"
 AUTHOR_EMAIL = "jesford@uw.edu"
 MAINTAINER = "Jes Ford"
 MAINTAINER_EMAIL = "jesford@uw.edu"
-URL = 'http://github.com/jesford/cluster-lensing'
-DOWNLOAD_URL = 'http://github.com/jesford/cluster-lensing'
+URL = 'http://github.com/jesford/clusterlensing'
+DOWNLOAD_URL = 'http://github.com/jesford/clusterlensing'
 LICENSE = 'MIT'
-VERSION = '0.0.1'
+VERSION = '0.1.0'
 
 setup(name=NAME,
-      version=VERSION,
-      description=DESCRIPTION,
-      long_description=LONG_DESCRIPTION,
-      author=AUTHOR,
-      author_email=AUTHOR_EMAIL,
-      maintainer=MAINTAINER,
-      maintainer_email=MAINTAINER_EMAIL,
-      url=URL,
-      download_url=DOWNLOAD_URL,
-      license=LICENSE,
-      packages=['cluster-lensing'],
-      classifiers=[
+    version=VERSION,
+    description=DESCRIPTION,
+    long_description=LONG_DESCRIPTION,
+    author=AUTHOR,
+    author_email=AUTHOR_EMAIL,
+    maintainer=MAINTAINER,
+    maintainer_email=MAINTAINER_EMAIL,
+    url=URL,
+    download_url=DOWNLOAD_URL,
+    license=LICENSE,
+    packages=['clusterlensing'],
+    classifiers=[
         'Development Status :: 4 - Beta',
         'Environment :: Console',
         'Intended Audience :: Science/Research',
         'License :: OSI Approved :: MIT License',
         'Natural Language :: English',
-        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 2.7'
+        ],
+    install_requires=[
+        "numpy",
+        "scipy",
+        "pandas",
+        "astropy"
+        ]
      )
