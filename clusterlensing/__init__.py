58bf02a11d89eb1a90dfc4469e2042ca5e64d3f2
Jes Ford <jesford@uw.edu>
2016-02-18 14:29:11 -0800

    add version

--- clusterlensing/__init__.py
+++ clusterlensing/__init__.py
@@ -2,3 +2,5 @@ from .clusters import ClusterEnsemble  # noqa
 from .nfw import SurfaceMassDensity    # noqa
 import cofm                            # noqa
 import halobias                        # noqa
+
+__version__ = '0.1.0'                  # noqa
