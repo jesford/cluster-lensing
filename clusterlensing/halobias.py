2b172b1320d1d494530475efd64a171ab686026c
Jes Ford <jesford@uw.edu>
2016-02-18 10:31:00 -0800

    docstring: add reference, discuss z>0 scaling

--- clusterlensing/halobias.py
+++ clusterlensing/halobias.py
@@ -14,7 +14,7 @@ def bias(mass, z, h=h, Om_M=Om_M, Om_L=Om_L):
     mass : ndarray or float
         Halo mass to calculate bias for.
     z : ndarray or float
-        Halo z, same type and size as mass_halo.
+        Halo z, same type and size as mass.
     h : float, optional
         Hubble parameter, defaults to astropy.cosmology.Planck13.h
     Om_M : float, optional
@@ -27,7 +27,17 @@ def bias(mass, z, h=h, Om_M=Om_M, Om_L=Om_L):
     ----------
     ndarray or float
         Returns the halo bias, of same type and size as input mass_halo and
-        z_halo. Calculated according to Seljak & Warren 2004.
+        z_halo. Calculated according to Seljak & Warren 2004 for z = 0.
+        For halo z > 0, the non-linear mass is adjusted using the input
+        cosmological parameters.
+
+    References
+    ----------
+    Based on fitting formula derived from simulations in:
+
+    U. Seljak and M.S. Warren, "Large-scale bias and stochasticity of
+    haloes and dark matter," Monthly Notices of the Royal Astronomical
+    Society, Volume 355, Issue 1, pp. 129-136 (2004).
     """
     M_nl_0 = (8.73 / h) * (10. ** 12.)         # nonlinear mass today [M_sun]
     M_nl = M_nl_0 * (Om_M + Om_L / ((1. + z) ** 3.))  # scaled to z_lens
