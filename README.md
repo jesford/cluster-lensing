68a1bfa6f04d9a1135dbb2225e7f89d4c81d2cfb
Jes Ford <jesford@uw.edu>
2016-02-18 15:04:38 -0800

    set up nojekyll for pages

--- README.md
+++
@@ -1,3 +0,0 @@
-## Weak Lensing Profile Fitting Tools
-
-A Python repository in progress for modules that are helpful in fitting weak lensing shear or magnification profiles. 
--- halobias.py
+++
@@ -1,7 +0,0 @@
-def bias(mass_halo, z_halo)
-"""Return halo bias, from Seljak & Warren 2004."""
-    M_nl_0 = (8.73/h) * 10.**12.         #nonlinear mass today [M_sun]
-    M_nl = M_nl_0 * (Om_M + Om_L/((1.+z_halo)**3.))  #scaled to z_lens
-    x = mass_halo/M_nl
-    b = 0.53 + 0.39*(x**0.45) + 0.13/(40.*x + 1.) + (5.e-4)*(x**1.5)
-    return b
