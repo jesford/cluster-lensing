33c8aca4c6d6403d9353bc22665520a9f8bd5840
Jes Ford <jesford@uw.edu>
2016-03-18 14:51:55 -0700

    no more relative imports

--- clusterlensing/__init__.py
+++ clusterlensing/__init__.py
@@ -1,6 +1,6 @@
 from .clusters import ClusterEnsemble  # noqa
 from .nfw import SurfaceMassDensity    # noqa
-import cofm                            # noqa
-import halobias                        # noqa
+import .cofm                           # noqa
+import .halobias                       # noqa
 
 __version__ = '0.1.1'                  # noqa
