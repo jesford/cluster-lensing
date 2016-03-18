d44623b92b1542b5288119af50f72513f464a239
Jes Ford <jesford@uw.edu>
2016-03-18 14:57:52 -0700

    fixing absolute imports

--- clusterlensing/__init__.py
+++ clusterlensing/__init__.py
@@ -1,6 +1,5 @@
-from .clusters import ClusterEnsemble  # noqa
-from .nfw import SurfaceMassDensity    # noqa
-import .cofm                           # noqa
-import .halobias                       # noqa
+from .clusters import ClusterEnsemble               # noqa
+from .nfw import SurfaceMassDensity                 # noqa
+from .cofm import c_Prada, c_DuttonMaccio, c_Duffy  # noqa
 
 __version__ = '0.1.1'                  # noqa
